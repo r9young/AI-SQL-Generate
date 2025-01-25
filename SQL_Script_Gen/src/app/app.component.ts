124ab844e459c66dcb36d70465d4a105b039dc29
xianjun Yang <xianjunyang@xianjuns-MacBook-Pro.local>
2025-01-25 16:07:17 +1000

    update

--- SQL_Script_Gen/src/app/app.component.ts
+++ SQL_Script_Gen/src/app/app.component.ts
@@ -1,38 +1,28 @@
-// import { Component } from '@angular/core';
-
-// @Component({
-//   selector: 'app-root',
-//   templateUrl: './app.component.html',
-//   styleUrls: ['./app.component.css']
-// })
-// export class AppComponent {
-//   title = 'sql-script-gen';
-// }
-
-// import { NgModule } from '@angular/core';
-// import { BrowserModule } from '@angular/platform-browser';
-// import { AppComponent } from './app.component';
-// import { PromptInputComponent } from './prompt-input/prompt-input.component';
-
-// @NgModule({
-//   declarations: [
-//     AppComponent,
-//     PromptInputComponent
-//   ],
-//   imports: [
-//     BrowserModule,
-//   ],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule {}
-
 import { Component } from '@angular/core';
-import { PromptInputComponent } from './prompt-input/prompt-input.component';
 
 @Component({
-  standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [PromptInputComponent],
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'sql-script-gen';
+}
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { PromptInputComponent } from './prompt-input/prompt-input.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PromptInputComponent
+  ],
+  imports: [
+    BrowserModule,
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
+
